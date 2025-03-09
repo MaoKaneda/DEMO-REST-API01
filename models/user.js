000ef156f1b5cdaec7bd4868b64b5ec318d81686
Mao Kaneda <maddieqqie@gmail.com>
2025-03-09 23:39:16 +0900

    実験として記述していた不要なもの削除

--- models/user.js
+++ models/user.js
@@ -21,32 +21,3 @@ what i want to do is
 Cursorで書いてもらったものがUdemyと自分ので違うから
 現在差分を確認
 
-
-
-module.exports = {
-  createUser,
-  findUserByEmail,
-} 
-
-const createUser = (userData) => {
-  console.log('creating user', userData);
-  return { id: 'someUniqueId', ...userData };    
-}
-
-const findUserByEmail = (email) => {
-  console.log('finding user by email', email);  
-  return { id: 'someUniqueId', email: email };
-}
-
-module.exports = {
-  createUser,
-  findUserByEmail,
-}
-
-const createUser = (userData) => {
-  console.log('creating user', userData);
-  return { id: 'someUniqueId', ...userData };    
-}
-
-
-
